b0b950d5093ef60de417f4669e9061690f77ea70
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2021-06-30 18:14:42 +0100

    Update build_data.py

--- scripts/build_data.py
+++ scripts/build_data.py
@@ -2,7 +2,8 @@ from sdg.open_sdg import open_sdg_build
 
 def alter_meta(meta):
     # Drop an reporting_status field in the metadata.
-    del meta['reporting_status']
+    if 'reporting_status' in meta:
+        del meta['reporting_status']
     return meta
 
 open_sdg_build(config='config_data.yml', alter_meta=alter_meta)
